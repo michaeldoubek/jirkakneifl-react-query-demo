d94d21ec850bf1695840dadd2b15f955a85fdf62
micha <michal@doubkovi.cz>
2022-12-20 13:08:42 +0100

    Connect frontend with backend using fetch API.

--- frontend/src/pages/Detail.tsx
+++ frontend/src/pages/Detail.tsx
@@ -1,15 +1,65 @@
-import { useState } from 'react';
+import { FormEvent, useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 function Detail() {
   const { id } = useParams();
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
 
+  useEffect(() => {
+    const loadPost = async () => {
+      const response = await fetch(`http://localhost:3000/${id}`);
+
+      if (response.ok) {
+        const post = await response.json();
+
+        setTitle(post.title);
+        setContent(post.content);
+      }
+    };
+
+    if (id !== 'new') loadPost();
+  }, []);
+
+  const updatePost = async () => {
+    const response = await fetch(`http://localhost:3000/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ title, content }),
+    });
+
+    if (response.ok) {
+      alert('Post updated!');
+    }
+  };
+
+  const createPost = async () => {
+    const response = await fetch(`http://localhost:3000/`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ title, content }),
+    });
+
+    if (response.ok) {
+      alert('Post created!');
+    }
+  };
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+
+    if (id !== 'new') updatePost();
+    else createPost();
+  };
+
   return (
     <div>
       <h1>Detail</h1>
       <p>id: {id}</p>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor="title">Title:</label>
           <input type="text" name="title" id="title" value={title} onChange={(e) => setTitle(e.target.value)} />
--- frontend/src/pages/List.tsx
+++ frontend/src/pages/List.tsx
@@ -1,9 +1,22 @@
 import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 function List() {
+  const [posts, setPosts] = useState<any[]>([]);
+
+  useEffect(() => {
+    const load = async () => {
+      const response = await fetch('http://localhost:3000/');
+      if (response.ok) setPosts(await response.json());
+    };
+
+    load();
+  }, []);
+
   return (
     <div>
-      <h1>Detail</h1>
+      <h1>List posts</h1>
+      <Link to="/new">New post</Link>
       <table>
         <thead>
           <tr>
@@ -13,15 +26,17 @@ function List() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>1</td>
-            <td>First post</td>
-            <td>
-              <Link to="/1">
-                <button>Detail</button>
-              </Link>
-            </td>
-          </tr>
+          {posts.map((post) => (
+            <tr>
+              <td>{post.id}</td>
+              <td>{post.title}</td>
+              <td>
+                <Link to={`/${post.id}`}>
+                  <button>Detail</button>
+                </Link>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
